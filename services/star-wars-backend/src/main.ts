df4e73a8ab8e7ba319ff86313b5acf39db6b92a4
Kacper Smal <kacpersmal@gmail.com>
2025-06-26 16:18:02 +0200

    init class validator

--- services/star-wars-backend/src/main.ts
+++ services/star-wars-backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ConfigureSwagger from 'src/common/swagger/init-swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   ConfigureSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
